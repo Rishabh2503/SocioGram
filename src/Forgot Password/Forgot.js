fdf113e896ee729e400e6a38558e449a2112190b
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-01-22 22:05:54 +0530

    Completed forgot password page validations

--- src/Forgot Password/Forgot.js
+++ src/Forgot Password/Forgot.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {useNavigate} from "react-router-dom";
 import "./Forgot.css";
 
 const Forgot = () => {
 
     const Navhandler= useNavigate();
+    const [email,setEmail] = useState('');
+
+    function handleEmail(e) {
+      if((/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/).test(e.target.value) || e.target.value === "")
+      { 
+        setEmail(e.target.value);
+        document.getElementById("signupInvEmail").style.visibility = "hidden";
+        if(e.target.value==="")
+        document.getElementById("email").style.borderColor = "black";
+        else
+        document.getElementById("email").style.borderColor = "#66DF98";
+      }
+      else 
+      {
+        document.getElementById("signupInvEmail").style.visibility = "visible";
+        document.getElementById("email").style.borderColor = "#CF6679";
+      }
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+    }
 
   return (
     <div id="box">
             <p id="head">Trouble logging in?</p>
             <p id='content'>Enter your email and we'll send you an otp to get back into your account.</p>
-            <div id="place"><input placeholder='Enter your email address'/></div>
-            <div id="sign_btn">Send OTP</div>
+            <div id="place"><input placeholder='Enter your email address'  onChange={handleEmail} id="email"  type="email" required/>
+            <p className='error' id="signupInvEmail">Invalid Email Address</p></div>
+            <div id="sign_btn" onClick={handleSubmit} >Send OTP</div>
             <div id="footer_msg">Create a new account.<span onClick={() => Navhandler('/signup')}>Sign Up</span></div>
         
     </div>
