9907240179509f4644b779239738e521f44dc9c7
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-01-22 21:59:31 +0530

    Completed signup form validations

--- src/Sign Up/SignUp.jsx
+++ src/Sign Up/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Heading from '../components/Heading'
 import "./SignUp.css";
 import { FcGoogle } from "react-icons/fc";
@@ -8,30 +8,62 @@ import signimg from "../images/SignImage.svg";
 const SignUp = () => {
 
     const Navhandler= useNavigate();
+    const [email,setEmail] = useState('');
+    const [uname,setUname] = useState('');
 
+    function handleEmail(e) {
+      if((/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/).test(e.target.value) || e.target.value === "")
+      { 
+        setEmail(e.target.value);
+        document.getElementById("signupInvEmail").style.visibility = "hidden";
+        if(e.target.value==="")
+        document.getElementById("email").style.borderColor = "black";
+        else
+        document.getElementById("email").style.borderColor = "#66DF98";
+      }
+      else 
+      {
+        document.getElementById("signupInvEmail").style.visibility = "visible";
+        document.getElementById("email").style.borderColor = "#CF6679";
+      }
+    }
+
+    function handleUname(e) {
+      if((/^[a-zA-Z][a-zA-Z ]*$/).test(e.target.value) || e.target.value === "")
+      { 
+        setEmail(e.target.value);
+        document.getElementById("signupInvName").style.visibility = "hidden";
+        if(e.target.value==="")
+        document.getElementById("uname").style.borderColor = "black";
+        else
+        document.getElementById("uname").style.borderColor = "#66DF98";
+      }
+      else 
+      {
+        document.getElementById("signupInvName").style.visibility = "visible";
+        document.getElementById("uname").style.borderColor = "#CF6679";
+      }
+    }
+    
+    const handleSubmit = (e) => {
+      e.preventDefault();
+    }
+    
   return (
     <div>
         <Heading />
         <img src={signimg} alt="LogIn-img" id="SignImg" />
         <div id="sign-up">
-          <p id="head">Socio Gram</p>
+          <p id="headi">Socio Gram</p>
           <div>
-            <input placeholder='Enter your email address'/>
-            <p className="error" >Enter valid Email Address</p>
+            <input placeholder='Enter your email address'  onChange={handleEmail} id="email"  type="email" required/>
+            <p className="error" id="signupInvEmail">Enter valid Email Address</p>
           </div>
           <div>
-            <input placeholder='Enter your Full Name'/>
-            <p className="error" >Enter valid Name</p>
+            <input placeholder='Enter your Full Name'  onChange={handleUname} id="uname"  type="uname" required/>
+            <p className="error" id="signupInvName">Enter valid Name</p>
           </div>
-          <div>
-            <input placeholder='Enter your Username'/>
-            <p className="error" >Enter valid Username</p>
-          </div>
-          <div>
-            <input placeholder='Enter your Password' type="password"/>
-            <p className="error" >Password should be more than 8 characters.</p>
-          </div>
-          <div id="sign_btn">Sign Up</div>
+          <div id="sign_btn" onClick={handleSubmit}>Sign Up</div>
           <div id="line"></div>
           <div id="google">
         <FcGoogle /> Continue with Google{" "}
